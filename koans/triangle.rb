717fb408ee1422e5ba69678474a8369e2deea47b
BoDonkey <robert.means1969@gmail.com>
2015-11-19 11:30:35 -0500

    Finish about_iteration.rb

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,7 +14,12 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  raise TriangleError if a < 1 || b < 1 || c < 1
+  check_var = [a, b, c].sort
+  raise TriangleError unless check_var[0] + check_var[1] > check_var[2]
+  return :equilateral if a == b && a == c
+  return :scalene if a != b && a != c && b != c
+  :isosceles
 end
 
 # Error class used in part 2.  No need to change this code.
